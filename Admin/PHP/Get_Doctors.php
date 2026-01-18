c05d6f8f473fa5c97506376f375de04671630d28
Musfiq009 <musfiq.rahat19@gmail.com>
2026-01-19 03:22:43 +0600

    Added view all doctor backend

---
+++ Admin/PHP/Get_Doctors.php
@@ -0,0 +1,36 @@
+<?php
+include "../DB/configDB.php";
+
+$sql = "SELECT * FROM doctors ORDER BY doctor_id DESC";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+?>
+
+<div id="doctor-<?php echo $row['doctor_id']; ?>" class="doctor-card">
+
+    <img src="../Images/<?php echo $row['photo']; ?>" alt="Doctor">
+
+    <h3><?php echo $row['full_name']; ?></h3>
+    <p><strong>Degree:</strong> <?php echo $row['degree']; ?></p>
+    <p><strong>Specialization:</strong> <?php echo $row['specialization']; ?></p>
+    <p><strong>Status:</strong> 
+        <span style="color:<?php echo $row['status']=='Available'?'green':'red'; ?>">
+            <?php echo $row['status']; ?>
+        </span>
+    </p>
+
+   <div class="card-buttons">
+    <a href="edit_doctor.php?id=<?php echo $row['doctor_id']; ?>" 
+       target="_blank" 
+       class="btn-edit">
+        Edit
+    </a>
+
+    <a href="#" 
+        class="btn-delete">
+        Delete
+    </a>
+</div>
+</div>
+<?php } ?>
